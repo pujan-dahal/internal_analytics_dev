2a707415598da952a93994f5bab6e87d0b45d81d
Pujan Dahal <pujan.dahal@fusemachines.com>
2023-03-02 17:23:49 +0545

    testing

---
+++ models/example/test_fruits.sql
@@ -0,0 +1,72 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+
+
+-- with source_table as
+-- (
+-- 	select * from {{ source('_airbyte_raw', '_airbyte_raw_form_responses_1') }} 
+-- ),
+-- extracted_json as
+-- (
+-- select
+--     jsonb_extract_path_text(_airbyte_data, 'Day') as day,
+--     jsonb_extract_path_text(_airbyte_data, 'Timestamp') as timestamp,
+--     jsonb_extract_path_text(_airbyte_data, '1. Your Shift') as shift,
+--     jsonb_extract_path_text(_airbyte_data, 'Email address') as email,
+-- 	jsonb_extract_path_text(_airbyte_data, 'Employee Name') as employee_name,
+-- 	jsonb_extract_path_text(_airbyte_data, 'Main Department') as main_department,
+-- 	jsonb_extract_path_text(_airbyte_data, 'Sub Department') as sub_department,
+-- 	jsonb_extract_path_text(_airbyte_data, '2. Food Coupon ID (write "00" if not taken)') as food_coupon_id,
+-- 	jsonb_extract_path_text(_airbyte_data, '3. Will you be using the company drop off service tonight ?') as drop_off_required
+-- from source_table
+-- ),
+-- final_table as
+-- (
+-- select
+-- 	day,
+-- 	to_timestamp(timestamp, 'dd/mm/yyyy HH24:MI:SS') as timestamp,
+-- 	shift,
+-- 	email,
+-- 	employee_name,
+-- 	main_department,
+-- 	sub_department,
+-- 	food_coupon_id::integer as food_coupon_id,
+-- 	drop_off_required
+-- from extracted_json
+-- )
+
+-- select * from final_table
+
+with fruits as
+(
+	select
+		'apple' as fruit,
+		10 as quantity
+	union all
+	select
+		'banana' as fruit,
+		25 as quantity
+	union all
+	select
+		'apple' as fruit,
+		30 as quantity
+)
+select
+	fruit,
+	avg(quantity) as avg_quantity
+from fruits
+group by 1
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
