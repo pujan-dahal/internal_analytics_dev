fe43ae7e6a1b8b0685266f43a6f226924fbec2f8
Pujan Dahal <pujan.dahal@fusemachines.com>
2023-03-02 16:37:09 +0545

    minor change to form responses json extract

--- models/example/form_responses_raw.sql
+++ models/example/form_responses_raw.sql
@@ -13,7 +13,7 @@
 
 with source_table as
 (
-	select * from {{ source('_airbyte_raw', '_airbyte_raw_form_responses_1')}}
+	select * from {{ source_table('_airbyte_raw', '_airbyte_raw_form_responses_1')}} 
 ),
 extracted_json as
 (
@@ -22,13 +22,13 @@ select
     jsonb_extract_path_text(_airbyte_data, 'Timestamp') as timestamp,
     jsonb_extract_path_text(_airbyte_data, '1. Your Shift') as shift,
     jsonb_extract_path_text(_airbyte_data, 'Email address') as email,
-	jsonb_extract_path_text(_airbyte_data, 'Employee name') as employee_name,
-	jsonb_array_elements_text(_airbyte_data, 'Main Department') as main_department,
-	jsonb_array_elements_text(_airbyte_data, 'Sub Department') as sub_department,
-	jsonb_extract_path_text(_airbyte_data, '2. Food Coupon ID (write \"00\" if not taken)') as food_coupon_id,
+	jsonb_extract_path_text(_airbyte_data, 'Employee Name') as employee_name,
+	jsonb_extract_path_text(_airbyte_data, 'Main Department') as main_department,
+	jsonb_extract_path_text(_airbyte_data, 'Sub Department') as sub_department,
+	jsonb_extract_path_text(_airbyte_data, '2. Food Coupon ID (write "00" if not taken)') as food_coupon_id,
 	jsonb_extract_path_text(_airbyte_data, '3. Will you be using the company drop off service tonight ?') as drop_off_required
 from source_table
-),
+)
 
 select * from extracted_json
 
