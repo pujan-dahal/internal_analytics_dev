43dc3d2bb2d40dedd3f8bd7a7c60b89580147ba8
Pujan Dahal <pujan.dahal@fusemachines.com>
2023-03-02 16:47:30 +0545

    modified transformation query

--- models/example/form_responses_raw.sql
+++ models/example/form_responses_raw.sql
@@ -13,7 +13,7 @@
 
 with source_table as
 (
-	select * from {{ source_table('_airbyte_raw', '_airbyte_raw_form_responses_1')}} 
+	select * from {{ source('_airbyte_raw', '_airbyte_raw_form_responses_1') }} 
 ),
 extracted_json as
 (
@@ -28,9 +28,23 @@ select
 	jsonb_extract_path_text(_airbyte_data, '2. Food Coupon ID (write "00" if not taken)') as food_coupon_id,
 	jsonb_extract_path_text(_airbyte_data, '3. Will you be using the company drop off service tonight ?') as drop_off_required
 from source_table
+),
+final_table as
+(
+select
+	day,
+	to_timestamp(timestamp, 'dd/mm/yyyy HH24:MI:SS') as timestamp,
+	shift,
+	email,
+	employee_name,
+	main_department,
+	sub_department,
+	food_coupon_id::integer as food_coupon_id,
+	drop_off_required
+from extracted_json
 )
 
-select * from extracted_json
+select * from final_table
 
 /*
     Uncomment the line below to remove records with null `id` values
